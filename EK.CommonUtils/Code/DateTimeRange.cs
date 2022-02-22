a1675029fe0fce29f5b0bb8b6835ae9358965576
null <fake@fake12345.com>
2022-02-23 00:49:16 +0100

    DateTimeRange - start>end parameter validation

--- EK.CommonUtils/Code/DateTimeRange.cs
+++ EK.CommonUtils/Code/DateTimeRange.cs
@@ -6,7 +6,12 @@ public sealed record DateTimeRange
     {
         if (start.Kind != end.Kind)
         {
-            throw new ArgumentException($"The {nameof(DateTime.Kind)} property in both parameters must have the same value.");
+            throw new ArgumentException($"The '{nameof(DateTime.Kind)}' property in both parameters must have the same value.");
+        }
+
+        if (start > end)
+        {
+            throw new ArgumentException($"The '{nameof(start)}' parameter cannot be greater than the '{nameof(end)}' parameter.", nameof(start));
         }
 
         Start = start;
