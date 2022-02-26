a65bbc7ac4c4ff85049db5346e54e7dfb72e4dab
null <fake@fake12345.com>
2022-02-26 22:55:13 +0100

    Added Truncate extension method

---
+++ EK.CommonUtils.Tests/Code/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using EK.CommonUtils.Extensions;
+using Xunit;
+
+namespace EK.CommonUtils.Tests.Extensions;
+
+public sealed class StringExtensionsTests
+{
+    [Theory]
+    [InlineData(null, 5, null)]
+    [InlineData("", 5, "")]
+    [InlineData("empty", 0, "")]
+    [InlineData("cut_me", 3, "cut")]
+    [InlineData("bigMaxLength", 50, "bigMaxLength")]
+    public void Test(string current, int maxLength, string expected)
+    {
+        string? truncated = current.Truncate(maxLength);
+
+        Assert.Equal(expected, truncated);
+    }
+}
---
+++ EK.CommonUtils/Code/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace EK.CommonUtils.Extensions;
+
+public static class StringExtensions
+{
+    public static string? Truncate(this string? me, int maxLength)
+    {
+        Guard.NotNegative(maxLength);
+
+        if (me is null) return null;
+        if (me.Length <= maxLength) return me;
+
+        return me[..maxLength];
+    }
+}
