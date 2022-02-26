55a7c32a566e7e027f31ae44b7a3be6bd8e0c3fa
null <fake@fake12345.com>
2022-02-26 03:48:09 +0100

    Added Disposable class

---
+++ EK.CommonUtils/Code/Disposable.cs
@@ -0,0 +1,27 @@
+﻿namespace EK.CommonUtils;
+
+public sealed class Disposable : IDisposable
+{
+    readonly Action disposeBody;
+
+    bool isDisposed = false;
+
+    //====== ctors
+
+    public Disposable(Action disposeBody)
+    {
+        this.disposeBody = Guard.NotNull(disposeBody);
+    }
+    
+    //====== IDisposable
+
+    public void Dispose()
+    {
+        if (isDisposed == false)
+        {
+            isDisposed = true;
+
+            disposeBody.Invoke();
+        }
+    }
+}
