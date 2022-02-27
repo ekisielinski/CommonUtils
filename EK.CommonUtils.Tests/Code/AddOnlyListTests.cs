50e25d27bb0b2d8c8195e2d67386f2e705affc8b
null <fake@fake12345.com>
2022-02-27 15:34:07 +0100

    Added AddOnlyList class

---
+++ EK.CommonUtils.Tests/Code/AddOnlyListTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace EK.CommonUtils.Tests;
+
+public sealed class AddOnlyListTests
+{
+    [Fact]
+    public void Add_NullValue()
+    {
+        var sut = new AddOnlyList<string?>();
+
+        sut.Add("1");
+        sut.Add(null);
+        sut.Add("3");
+
+        Assert.Equal(3, sut.Count);
+        Assert.Null(sut.EnumerateCurrentState().ElementAt(1));
+    }
+
+    [Fact]
+    public void Count_UsingAdd()
+    {
+        var sut = new AddOnlyList<int>();
+        
+        sut.Add(1);
+        sut.Add(2);
+        sut.Add(3);
+
+        Assert.Equal(3, sut.Count);
+    }
+
+    [Theory]
+    [InlineData(0, new int[] { })]
+    [InlineData(3, new int[] { 1, 2, 3 })]
+    [InlineData(5, new int[] { 1, 2, 3, 4, 5 })]
+    public void Count_UsingAddRange(int count, int[] collection)
+    {
+        var sut = new AddOnlyList<int>();
+
+        sut.AddRange(collection);
+
+        Assert.Equal(count, sut.Count);
+    }
+
+
+    [Fact]
+    public void EnumerateCurrentState()
+    {
+        var sut = new AddOnlyList<int>();
+
+        sut.Add(1);
+        sut.Add(2);
+        sut.Add(3);
+
+        IEnumerable<int> state = sut.EnumerateCurrentState();
+
+        sut.Add(4); // not included in 'state'
+        sut.Add(5);
+
+        Assert.Equal(3, state.Count());
+        Assert.Equal(1, state.ElementAt(0));
+        Assert.Equal(2, state.ElementAt(1));
+        Assert.Equal(3, state.ElementAt(2));
+    }
+}
---
+++ EK.CommonUtils/Code/AddOnlyList.cs
@@ -0,0 +1,55 @@
+﻿namespace EK.CommonUtils;
+
+public class AddOnlyList<T>
+{
+    readonly List<T> items = new();
+
+    protected readonly object sync = new();
+
+    //====== public properties
+
+    public int Count
+    {
+        get { lock (sync) { return items.Count; } }
+    }
+
+    //====== public methods
+
+    public virtual void Add(T item)
+    {
+        lock (sync)
+        {
+            items.Add(item);
+        }
+    }
+
+    public virtual void AddRange(IEnumerable<T> collection)
+    {
+        Guard.NotNull(collection);
+
+        lock (sync)
+        {
+            items.AddRange(collection);
+        }
+    }
+
+    public IEnumerable<T> EnumerateCurrentState()
+    {
+        int currentItemCount;
+
+        lock (sync)
+        {
+            currentItemCount = items.Count;
+        }
+
+        return Enumerate(currentItemCount);
+
+        IEnumerable<T> Enumerate(int itemCount)
+        {
+            for (int i = 0; i < itemCount; i++)
+            {
+                yield return items[i];
+            }
+        }
+    }
+}
