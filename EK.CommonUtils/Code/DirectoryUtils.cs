8451babf7f61dd4c3636ffaccc2daffb33f43ba2
null <fake@fake12345.com>
2022-02-27 14:48:13 +0100

    Added FileUtils and DirectoryUtils

---
+++ EK.CommonUtils/Code/DirectoryUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace EK.CommonUtils;
+
+public static class DirectoryUtils
+{
+    public static void DeleteIfExists(string dirPath)
+    {
+        Guard.NotNull(dirPath);
+
+        if (Directory.Exists(dirPath))
+        {
+            Directory.Delete(dirPath);
+        }
+    }
+}
---
+++ EK.CommonUtils/Code/FileUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace EK.CommonUtils;
+
+public static class FileUtils
+{
+    public static void DeleteIfExists(string filePath)
+    {
+        Guard.NotNull(filePath);
+
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+    
+    public static DataSize GetSize(string filePath)
+    {
+        Guard.NotNull(filePath);
+
+        long size = new FileInfo(filePath).Length;
+
+        return new DataSize(size);
+    }
+}
