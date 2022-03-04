875b39ccbdce632f3883b98de04613ff4fe50b4e
null <fake@fake12345.com>
2022-03-04 19:19:45 +0100

    Added IEnumerableExtensions class

---
+++ EK.CommonUtils/Code/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace EK.CommonUtils.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> me, Action<T> method)
+    {
+        Guard.NotNull(me);
+        Guard.NotNull(method);
+
+        foreach (var item in me)
+        {
+            method.Invoke(item);
+        }
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> me, Action<T, int> method)
+    {
+        Guard.NotNull(me);
+        Guard.NotNull(method);
+
+        int index = 0;
+
+        foreach (var item in me)
+        {
+            method.Invoke(item, index++);
+        }
+    }
+}
