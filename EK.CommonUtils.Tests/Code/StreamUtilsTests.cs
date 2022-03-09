8266bb6683e29f44a15763217e547c838fc3dc30
null <fake@fake12345.com>
2022-03-09 21:17:02 +0100

    Added StreamUtils

---
+++ EK.CommonUtils.Tests/Code/StreamUtilsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace EK.CommonUtils.Tests;
+
+public sealed class StreamUtilsTests
+{
+    const string TestMessage = "Hello!123";
+
+    [Fact]
+    public void ToStream_AsciiString()
+    {
+        Stream stream = StreamUtils.ToStream(TestMessage, Encoding.ASCII);
+
+        Assert.NotNull(stream);
+        Assert.Equal(TestMessage.Length, stream.Length);
+
+        const int Position = 5;
+
+        stream.Position = Position;
+        int data = stream.ReadByte();
+
+        Assert.Equal(TestMessage[Position], data);
+    }
+
+    [Fact]
+    public void ToString_AsciiString_StreamGetsDisposed()
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes(TestMessage);
+        var ms = new MemoryStream(bytes);
+        
+        string actualMessage = StreamUtils.ToString(ms, encoding: Encoding.ASCII);
+
+        Assert.Equal(TestMessage, actualMessage);
+        Assert.Throws<ObjectDisposedException>(() => ms.Position = 0);
+    }
+}
---
+++ EK.CommonUtils/Code/StreamUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace EK.CommonUtils;
+
+public static class StreamUtils
+{
+    public static string ToString(Stream stream, Encoding encoding)
+    {
+        Guard.NotNull(stream);
+        Guard.NotNull(encoding);
+
+        using var reader = new StreamReader(stream, encoding);
+
+        string content = reader.ReadToEnd();
+
+        return content;
+    }
+
+    public static Stream ToStream(string content, Encoding encoding)
+    {
+        Guard.NotNull(content);
+        Guard.NotNull(encoding);
+
+        byte[] data = encoding.GetBytes(content);
+
+        return new MemoryStream(data);
+    }
+}
