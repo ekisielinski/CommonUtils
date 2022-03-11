b6011b8665fcea200da6cdbf603a5ac7e76deee4
null <fake@fake12345.com>
2022-03-11 23:07:20 +0100

    Added IEnumerableExtensionsTests

---
+++ EK.CommonUtils.Tests/Code/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using EK.CommonUtils.Extensions;
+using Xunit;
+
+namespace EK.CommonUtils.Tests.Extensions;
+
+public sealed class IEnumerableExtensionsTests
+{
+    [Fact]    
+    public void ForEach_WithoutIndex()
+    {
+        var items = new int[] { 1, 2, 3, 4, 5 };
+
+        var result = new List<int>();
+
+        items.ForEach(result.Add);
+
+        Assert.Equal(5, result.Count);
+        Assert.Equal(1, result[0]);
+        Assert.Equal(5, result[^1]);
+    }
+
+    [Fact]
+    public void ForEach_WithIndex()
+    {
+        var items = new int[] { 1, 2, 3, 4, 5 };
+
+        var result = new List<int>();
+
+        items.ForEach((x, index) => result.Add(index));
+
+        Assert.Equal(5, result.Count);
+        Assert.Equal(0, result[0]);
+        Assert.Equal(4, result[^1]);
+    }
+}
