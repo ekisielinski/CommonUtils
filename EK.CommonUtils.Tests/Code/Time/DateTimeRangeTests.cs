ea3806f93f5ebc6f84f467ba8886689bb342a88f
null <fake@fake12345.com>
2022-03-11 22:48:50 +0100

    Added DateTimeRange tests

---
+++ EK.CommonUtils.Tests/Code/Time/DateTimeRangeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EK.CommonUtils.Time;
+using Xunit;
+
+namespace EK.CommonUtils.Tests.Time;
+
+public sealed class DateTimeRangeTests
+{
+    [Fact]
+    public void Ctor_DifferentKinds_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var now = DateTime.Now;
+            var utcNow = now.ToUniversalTime();
+
+            new DateTimeRange(now, utcNow);
+        });
+    }
+
+    [Fact]
+    public void Ctor_EndTimeBeforeStartTime_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            var now = DateTime.Now;
+            var past = now - TimeSpan.FromSeconds(1);
+
+            new DateTimeRange(now, past);
+        });
+    }
+}
