d434f875a374c8414ba4c9f20e40fec6a86663e5
null <fake@fake12345.com>
2022-03-22 21:26:54 +0100

    Added BitUtils class / tests

---
+++ EK.CommonUtils.Tests/Code/BitUtilsTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace EK.CommonUtils.Tests;
+
+public sealed class BitUtilsTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(ushort.MaxValue, ushort.MaxValue)]
+    [InlineData(0xFF00, 0x00FF)]
+    [InlineData(0x00FF, 0xFF00)]
+    [InlineData(0xF0F0, 0xF0F0)]
+    [InlineData(0xABCD, 0xCDAB)]
+    [InlineData(0b11001100_10101010, 0b10101010_11001100)]
+    public void ReverseBytes_UInt16(ushort input, ushort expected)
+    {
+        ushort actual = BitUtils.ReverseBytes(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(-1, -1)]
+    [InlineData(-256, 255)]
+    [InlineData(255, -256)]
+    [InlineData(-3856, -3856)]
+    [InlineData(-21555, -12885)]
+    [InlineData(-13098, -10548)]
+    public void ReverseBytes_Int16(short input, short expected)
+    {
+        short actual = BitUtils.ReverseBytes(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ EK.CommonUtils/Code/BitUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace EK.CommonUtils;
+
+public static class BitUtils
+{
+    public static ushort ReverseBytes(ushort value)
+    {
+        return (ushort)((value & 0x00FFu) << 8 | (value & 0xFF00u) >> 8);
+    }
+
+    public static short ReverseBytes(short value)
+    {
+        ushort v = (ushort)value;
+
+        return (short)((v & 0x00FFu) << 8 | (v & 0xFF00u) >> 8);
+    }
+}
