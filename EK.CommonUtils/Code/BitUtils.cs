cce3501a06aee86c69671de2c2038efb39fdb16d
null <fake@fake12345.com>
2022-04-10 21:08:23 +0200

    BitUtils - formatting

--- EK.CommonUtils/Code/BitUtils.cs
+++ EK.CommonUtils/Code/BitUtils.cs
@@ -4,35 +4,35 @@ public static class BitUtils
 {
     public static ushort ReverseBytes(ushort value)
     {
-        return (ushort)((value & 0x00FFu) << 8 | (value & 0xFF00u) >> 8);
+        return (ushort) ((value & 0x00FFu) << 8 | (value & 0xFF00u) >> 8);
     }
 
     public static short ReverseBytes(short value)
     {
         unchecked
         {
-            ushort v = (ushort)value;
+            ushort v = (ushort) value;
 
-            return (short)((v & 0x00FFu) << 8 | (v & 0xFF00u) >> 8);
+            return (short) ((v & 0x00FFu) << 8 | (v & 0xFF00u) >> 8);
         }
     }
 
     public static uint ReverseBytes(uint value)
     {
         value = value >> 16 | value << 16;
-        
-        return (value & 0xFF00FF00u) >> 8 | (value & 0x00FF00FF) << 8;
+
+        return (value & 0xFF00FF00u) >> 8 | (value & 0x00FF00FFu) << 8;
     }
 
     public static int ReverseBytes(int value)
     {
         unchecked
         {
-            uint v = (uint)value;
+            uint v = (uint) value;
 
             v = v >> 16 | v << 16;
 
-            return (int)((v & 0xFF00FF00u) >> 8 | (v & 0x00FF00FF) << 8);
+            return (int) ((v & 0xFF00FF00u) >> 8 | (v & 0x00FF00FFu) << 8);
         }
     }
 }
