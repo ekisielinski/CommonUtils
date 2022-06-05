6432c78e07a4d6283fdb895dd4f990f6c2aae053
null <fake@fake12345.com>
2022-06-05 17:49:42 +0200

    Added Duration struct

---
+++ EK.CommonUtils.Tests/Code/Time/DurationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace EK.CommonUtils.Tests.Time;
+
+public sealed class DurationTests
+{
+    [Fact]
+    public void Ctor_NegativeTimeSpan_ThrowException()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Duration(TimeSpan.FromTicks(-1)));
+    }
+
+    [Fact]
+    public void Ctor_PositiveTimeSpan_Pass()
+    {
+        _ = new Duration(TimeSpan.FromTicks(1));
+    }
+
+    [Fact]
+    public void Ctor_TimeSpanZero_Pass()
+    {
+        _ = new Duration(TimeSpan.Zero);
+    }
+}
---
+++ EK.CommonUtils/Code/Time/Duration.cs
@@ -0,0 +1,23 @@
+﻿namespace EK.CommonUtils.Tests.Time;
+
+public readonly record struct Duration
+{
+    public Duration(TimeSpan value)
+    {
+        Guard.NotNegative(value.Ticks);
+
+        Value = value;
+    }
+
+    //====== public properties
+
+    public TimeSpan Value { get; }
+
+    //====== public static properties
+
+    public static Duration Zero { get; } = new(TimeSpan.Zero);
+
+    //====== override: Object
+
+    public override string ToString() => Value.ToString();
+}
