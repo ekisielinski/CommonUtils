ba300fbf7f8143672fc559651862bc1dfd1512eb
null <fake@fake12345.com>
2022-11-11 16:10:24 +0100

    Added more empty actions

--- EK.CommonUtils/Code/Actions.cs
+++ EK.CommonUtils/Code/Actions.cs
@@ -5,4 +5,8 @@ public static class Actions
     public static void Empty() { }
 
     public static void Empty<T>(T _) { }
+
+    public static void Empty<T1, T2>(T1 _1, T2 _2) { }
+
+    public static void Empty<T1, T2, T3>(T1 _1, T2 _2, T3 _3) { }
 }
